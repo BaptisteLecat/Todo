2af9098aa34425c566df4a505d01308dce71de8d
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2020-11-16 13:23:08 +0100

    TaskForm
    Ajout de la view TaskForm

---
+++ view/taskForm.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaskForm</title>
+</head>
+<body>
+    <header>
+        <div class="header_title">
+            <h1>Création d'une Tâche</h1>
+        </div>
+    </header>
+    <main>
+        <form action="">
+            <h6>Todo de référence</h6>
+            <input type="text">
+        </form>
+    </main>
+</body>
+</html>
