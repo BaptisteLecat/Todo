0402fe02827f7a89080ee6ea0ccdc2edf1ae0146
Baptiste <baptiste.lecat44@gmail.com>
2021-01-04 23:30:52 +0100

    Update todoView.php

--- view/todoView.php
+++ view/todoView.php
@@ -1,127 +1,130 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="asset/css/todoView.css">
-    <link rel="stylesheet" href="asset/css/app.css">
-    <title>TODO</title>
-  </head>
-  <body>
+  <link rel="stylesheet" href="asset/css/todoView.css">
+  <link rel="stylesheet" href="asset/css/app.css">
+  <title>TODO</title>
+</head>
 
-    <main>
+<body>
+
+  <main>
 
 
-      <div class="container_todoView">
+    <div class="container_todoView">
 
-        <div class="box_todoView">
+      <div class="box_todoView">
 
-          <div class="box_icon">
-            <img class="img_theme" src="asset/todo_icon/house.png">
-          </div>
-
-          <div class="box_theme">
-            <h1 class="text_theme">Décoration Chambre</h1>
-          </div>
-
-          <p>6 Tâches</p>
-
-          <div class="progressBar">
-            <div class="line_progressBar"></div>
-          </div>
+        <div class="box_icon">
+          <img class="img_theme" src="asset/todo_icon/house.png">
         </div>
 
-        <div class="box_todoView">
-
-          <div class="box_icon">
-            <img class="img_theme" src="asset/todo_icon/shopping-cart.png">
-          </div>
-
-          <div class="box_theme">
-            <h1 class="text_theme">Course</h1>
-          </div>
-
-          <p>10 Tâches</p>
-
-          <div class="progressBar">
-            <div class="line_progressBar"></div>
-          </div>
+        <div class="box_theme">
+          <h1 class="text_theme">Décoration Chambre</h1>
         </div>
 
-        <div class="box_todoView">
+        <p>6 Tâches</p>
 
-          <div class="box_icon">
-            <img class="img_theme" src="asset/todo_icon/businessman.png">
-          </div>
-
-          <div class="box_theme">
-            <h1 class="text_theme">Travail</h1>
-          </div>
-
-          <p>12 Tâches</p>
-
-          <div class="progressBar">
-            <div class="line_progressBar"></div>
-          </div>
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
         </div>
-
-        <div class="box_todoView">
-
-          <div class="box_icon">
-            <img class="img_theme" src="asset/todo_icon/cake.png">
-          </div>
-
-          <div class="box_theme">
-            <h1 class="text_theme">Anniversaire</h1>
-          </div>
-
-          <p>18 Tâches</p>
-
-          <div class="progressBar">
-            <div class="line_progressBar"></div>
-          </div>
-        </div>
-
-        <div class="box_todoView">
-
-          <div class="box_icon">
-            <img class="img_theme" src="asset/todo_icon/aircraft.png">
-          </div>
-
-          <div class="box_theme">
-            <h1 class="text_theme">Voyage</h1>
-          </div>
-
-          <p>124 Tâches</p>
-
-          <div class="progressBar">
-            <div class="line_progressBar"></div>
-          </div>
-        </div>
-
-        <div class="box_todoView">
-
-          <div class="box_icon">
-            <img class="img_theme" src="asset/todo_icon/user.png">
-          </div>
-
-          <div class="box_theme">
-            <h1 class="text_theme">Personnel</h1>
-          </div>
-
-          <p>6 Tâches</p>
-
-          <div class="progressBar">
-            <div class="line_progressBar"></div>
-          </div>
-        </div>
-
       </div>
 
+      <div class="box_todoView">
 
-    </main>
+        <div class="box_icon">
+          <img class="img_theme" src="asset/todo_icon/shopping-cart.png">
+        </div>
 
-  </body>
+        <div class="box_theme">
+          <h1 class="text_theme">Course</h1>
+        </div>
+
+        <p>10 Tâches</p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+
+      <div class="box_todoView">
+
+        <div class="box_icon">
+          <img class="img_theme" src="asset/todo_icon/businessman.png">
+        </div>
+
+        <div class="box_theme">
+          <h1 class="text_theme">Travail</h1>
+        </div>
+
+        <p>12 Tâches</p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+
+      <div class="box_todoView">
+
+        <div class="box_icon">
+          <img class="img_theme" src="asset/todo_icon/cake.png">
+        </div>
+
+        <div class="box_theme">
+          <h1 class="text_theme">Anniversaire</h1>
+        </div>
+
+        <p>18 Tâches</p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+
+      <div class="box_todoView">
+
+        <div class="box_icon">
+          <img class="img_theme" src="asset/todo_icon/aircraft.png">
+        </div>
+
+        <div class="box_theme">
+          <h1 class="text_theme">Voyage</h1>
+        </div>
+
+        <p>124 Tâches</p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+
+      <div class="box_todoView">
+
+        <div class="box_icon">
+          <img class="img_theme" src="asset/todo_icon/user.png">
+        </div>
+
+        <div class="box_theme">
+          <h1 class="text_theme">Personnel</h1>
+        </div>
+
+        <p>6 Tâches</p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+
+    </div>
+
+
+  </main>
+
+</body>
+
 </html>
