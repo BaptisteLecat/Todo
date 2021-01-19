2d1436faf63a6cb4c0e3e33de2eaf1732873b485
Baptiste <baptiste.lecat44@gmail.com>
2021-01-19 23:36:49 +0100

    Update taskForm.php
    Correction de bug form

--- public/form/taskForm.php
+++ public/form/taskForm.php
@@ -2,17 +2,17 @@
 
 if (isset($_POST["todo-selector"])) { //Si le formulaire a été soumis.
     $errorId = true; //Variable permettant de verifier que l'id du todo-selector existe bien.
-    foreach ($user->getListTodo() as $todo) {
+    foreach ($this->user->getListTodo() as $todo) {
 
         if ($todo->getId() == $_POST["todo-selector"]) { //L'id est valide.
             $errorId = false;
-            $resultInsertTask = $taskManager->insertTask($_POST["content"], $_POST["date"], $_POST["time"], $todo);
+            $resultInsertTask = $this->taskManager->insertTask($_POST["content"], $_POST["date"], $_POST["time"], $todo);
 
             //Affichage de la messageBox success ou error.
             if ($resultInsertTask["success"] == 1) {
-                $messageBox = new MessageBox("Félicitation, vous avez désormais une tâche supplémentaire à effectuer !", "validate");
+                //$this->messageBox = "Félicitation, vous avez désormais une tâche supplémentaire à effectuer !", "validate");
             } else {
-                $messageBox = new MessageBox("Ohoh, il semblerait qu'un problème soit survenue !", "error");
+                //$this->messageBox = new MessageBox("Ohoh, il semblerait qu'un problème soit survenue !", "error");
             }
             break;
         }
@@ -20,7 +20,7 @@ if (isset($_POST["todo-selector"])) { //Si le formulaire a été soumis.
 
     //L'id du todo-selector n'existe pas.
     if ($errorId == true) {
-        $messageBox = new MessageBox("Ohoh, le Todo sélectionné est inconnu !", "error");
+        //$messageBox = new MessageBox("Ohoh, le Todo sélectionné est inconnu !", "error");
     }
 }
 include("../view/form/taskForm.php");
