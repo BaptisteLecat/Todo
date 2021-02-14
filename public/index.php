d55bce03878e4cf74af7c7cb8766a0d5fc7cc9ae
BuildTools <unconfigured@null.spigotmc.org>
2021-02-14 22:55:46 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -20,7 +20,6 @@ if (isset($_SESSION["User"])) {
                 break;
 
             case 'form':
-                var_dump("ccc ".$_GET["action"]);
                 if (isset($_GET["action"])) {
                     $controller->displayForm_TaskTodo($_GET["action"]);
                 } else {
