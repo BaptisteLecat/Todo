3c663452a70217d462643dbb063442f49e3a2e3d
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-03-08 14:06:36 +0100

    Modification des gestionnaire de logs

--- App/Model/TaskUpdatedManager.php
+++ App/Model/TaskUpdatedManager.php
@@ -4,7 +4,6 @@ namespace App\Model;
 
 use Exception;
 use App\Model\Entity\Task;
-use App\Model\Entity\TaskArchived;
 use App\Model\Entity\TaskUpdated;
 use App\Model\Entity\Todo;
 use App\PdoFactory;
@@ -17,17 +16,18 @@ use App\PdoFactory;
  * @version 1.0.0
  */
 class TaskUpdatedManager
-{    
+{
     /**
-     * loadTaskArchiveFromTodo
+     * loadTaskUpdatedFromTodo
      * Set taskArchiveObject of a task if it was archived.
      * Called for displaying the logs.
      *
      * @param  mixed $todoObject
      * @return void
      */
-    public static function loadTaskUpdatedFromTodo(Todo $todoObject){
-        try{
+    public static function loadTaskUpdatedFromTodo(Todo $todoObject)
+    {
+        try {
             $request = PdoFactory::getPdo()->prepare("SELECT taskupdate_date.date_update, user.name_user, task.id_task
             FROM user, task_update, taskupdate_date, todo, task 
             WHERE user.id_user = task_update.id_user
@@ -36,31 +36,30 @@ class TaskUpdatedManager
             AND task.id_todo = todo.id_todo 
             AND todo.id_todo = :id_todo 
             AND DAYOFYEAR(taskupdate_date.date_update) >= DAYOFYEAR(NOW()) - 10");
-             $request->execute(array(':id_todo' => $todoObject->getId()));
-             while ($result = $request->fetch()) {
-                $taskArchived = new TaskArchived($result["date_update"], $result["name_user"]);
+            $request->execute(array(':id_todo' => $todoObject->getId()));
+            while ($result = $request->fetch()) {
                 foreach ($todoObject->getList_Task() as $task) {
-                    if($task->getId() == $result["id_task"]){
-                        $task->setTaskArchivedObject($taskArchived);
+                    if ($task->getId() == $result["id_task"]) {
+                        $taskUpdated = new TaskUpdated($result["date_update"], $result["name_user"], $task);
                         break;
                     }
                 }
-             }
-        }catch(Exception $e){
+            }
+        } catch (Exception $e) {
             throw new Exception($e);
         }
     }
 
     /**
      * loadTaskUpdatedFromTask
-     * Set taskUpdatedObject of a task if it was updated.
      * Called when a task was just updated.
      *
      * @param  mixed $taskObject
      * @return void
      */
-    public static function loadTaskUpdatedFromTask(Task $taskObject){
-        try{
+    public static function loadTaskUpdatedFromTask(Task $taskObject)
+    {
+        try {
             $request = PdoFactory::getPdo()->prepare("SELECT taskupdate_date.date_update, user.name_user 
             FROM task, task_update, taskupdate_date, user 
             WHERE task.id_task = task_update.id_task 
@@ -69,11 +68,10 @@ class TaskUpdatedManager
             AND task.id_task = :id_task");
             $request->execute(array(':id_task' => $taskObject->getId()));
             $result = $request->fetch();
-            if($request->rowCount() > 0){
-                $taskUpdated = new TaskUpdated($result["date_update"], $result["name_user"]);
-                $taskObject->setTaskUpdatedObject($taskArchived);
+            if ($request->rowCount() > 0) {
+                $taskUpdated = new TaskUpdated($result["date_update"], $result["name_user"], $taskObject);
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             throw new Exception($e);
         }
     }
