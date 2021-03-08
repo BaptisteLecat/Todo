6ebec7524fb700a3a64de007a1be82c5feabccba
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-03-08 14:06:22 +0100

    Creation des log achieve

---
+++ App/Model/Entity/TaskAchieve.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Model\Entity;
+
+use JsonSerializable;
+
+class TaskAchive implements JsonSerializable
+{
+    private $date;
+    private $userName;
+    
+    private $list_task;
+
+    function __construct($date, string $userName) {
+        $this->date = $date;
+        $this->userName = $userName;
+        
+        $this->list_task = array();
+    }
+
+    public function jsonSerialize(){
+        return array(
+            "date" => $this->date,
+            "userName" => $this->userName,
+            "list_task" => $this->list_taskSerialize(),
+        );
+    }
+
+    public function getDate(){
+        return $this->date;
+    }
+
+    public function getUserName(){
+        return $this->userObject;
+    }
+
+    public function getList_Task(){
+        return $this->list_task;
+    }
+
+    public function addTask(Task $task){
+        array_push($this->list_task, $task);
+    }
+
+    public function removeTask($taskObject)
+    {
+        unset($this->list_task[array_search($taskObject, $this->list_task)]);
+    }
+
+    private function list_taskSerialize()
+    {
+        $list_taskSerialize = array();
+        foreach ($this->list_task as $task) {
+            array_push($list_taskSerialize, $task->jsonSerialize());
+        }
+
+        return $list_taskSerialize;
+    }
+}
---
+++ App/Model/TodoAchieveManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Model;
+
+use Exception;
+use App\Model\Entity\Task;
+use App\Model\Entity\TaskUpdated;
+use App\Model\Entity\Todo;
+use App\PdoFactory;
+
+/**
+ * TaskAchieveManager
+ * Static class for load Task requests.
+ * 
+ * @author Lecat Baptiste <baptiste.lecat44@gmail.com>
+ * @version 1.0.0
+ */
+class TaskAchieveManager
+{
+    /**
+     * loadTaskUpdatedFromTodo
+     * Set taskArchiveObject of a task if it was archived.
+     * Called for displaying the logs.
+     *
+     * @param  mixed $todoObject
+     * @return void
+     */
+    public static function loadTaskAchieveFromTodo(Todo $todoObject)
+    {
+        try {
+            $request = PdoFactory::getPdo()->prepare("SELECT taskachieve_date.date_achieve, user.name_user, task.id_task
+            FROM user, task_archive, taskachieve_date, todo, task 
+            WHERE user.id_user = task_archive.id_user
+            AND task_archive.id_date = taskachieve_date.id_achieve
+            AND task.id_task = task_archive.id_task 
+            AND task.id_todo = todo.id_todo 
+            AND todo.id_todo = :id_todo 
+            AND DAYOFYEAR(taskachieve_date.date_achieve) >= DAYOFYEAR(NOW()) - 10");
+            $request->execute(array(':id_todo' => $todoObject->getId()));
+            while ($result = $request->fetch()) {
+                foreach ($todoObject->getList_Task() as $task) {
+                    if ($task->getId() == $result["id_task"]) {
+                        $taskUpdated = new TaskUpdated($result["date_achieve"], $result["name_user"], $task);
+                        break;
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
+
+    /**
+     * loadTaskUpdatedFromTask
+     * Called when a task was just updated.
+     *
+     * @param  mixed $taskObject
+     * @return void
+     */
+    public static function loadTaskAchieveFromTask(Task $taskObject)
+    {
+        try {
+            $request = PdoFactory::getPdo()->prepare("SELECT taskachieve_date.date_achieve, user.name_user 
+            FROM task, task_archive, taskachieve_date, user 
+            WHERE task.id_task = task_archive.id_task 
+            AND taskachieve_date.id_achieve = task_archive.id_date 
+            AND user.id_user = task_archive.id_user 
+            AND task.id_task = :id_task");
+            $request->execute(array(':id_task' => $taskObject->getId()));
+            $result = $request->fetch();
+            if ($request->rowCount() > 0) {
+                $taskUpdated = new TaskUpdated($result["date_achieve"], $result["name_user"], $taskObject);
+            }
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
+}
