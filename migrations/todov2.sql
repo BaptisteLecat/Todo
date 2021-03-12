10d256528e9a19417f8a6778af6bd0947009dccc
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-03-12 12:25:43 +0100

    Update todov2.sql

--- migrations/todov2.sql
+++ migrations/todov2.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 10 mars 2021 à 23:10
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Généré le : ven. 12 mars 2021 à 11:25
+-- Version du serveur :  8.0.19
+-- Version de PHP : 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,20 +30,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `taskpriority`;
 CREATE TABLE IF NOT EXISTS `taskpriority` (
-  `id_priority` int(11) NOT NULL AUTO_INCREMENT,
+  `id_priority` int NOT NULL AUTO_INCREMENT,
   `label_priority` varchar(255) NOT NULL,
   `color_priority` varchar(25) NOT NULL,
   PRIMARY KEY (`id_priority`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `taskpriority`
 --
 
 INSERT INTO `taskpriority` (`id_priority`, `label_priority`, `color_priority`) VALUES
-(1, 'URGENT', '#F25F5C'),
-(2, 'A FAIRE', '#5C7AFF'),
-(3, 'TERMINE', '#FFFFFF');
+(1, 'URGENT', '#eb3723'),
+(2, 'A FAIRE', '5C7AFF'),
+(3, 'TERMINÉ', '#fff');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
