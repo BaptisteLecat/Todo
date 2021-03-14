a081c20207a29a8805b79c69469c4d107bba9a8a
Baptiste <baptiste.lecat44@gmail.com>
2021-03-14 23:39:37 +0100

    Fix mode archive
    Le mode archive empeche toute action autre que l'archivage.

--- public/js/todo/archivePressHandler.js
+++ public/js/todo/archivePressHandler.js
@@ -10,58 +10,62 @@ var touchduration = 400; //length of time we want the user to touch before we do
 var isArchived = false;
 
 function archivePressTouchStart() {
-    selectedElement = this;
-    startTimeMS = new Date().getTime();
-    timer = setTimeout(archiveFunction, touchduration);
+  selectedElement = this;
+  startTimeMS = new Date().getTime();
+  timer = setTimeout(archiveFunction, touchduration);
 }
 
 function archivePressTouchEnd() {
-    //stops short touches from firing the event
-    if (timer) {
-        //Simple click
-        if (getRemainingTime() < touchduration) {
-            //call achieveTaskFunction
-            if (isClickable && isArchived == false) {
-                achieveTask(selectedElement);
-            }
-        }
-        clearTimeout(timer); // clearTimeout, not cleartimeout..
+  //stops short touches from firing the event
+  if (timer) {
+    //Simple click
+    if (getRemainingTime() < touchduration) {
+      //call achieveTaskFunction
+      if (isClickable && isArchived == false) {
+        achieveTask(selectedElement);
+      }
     }
+    clearTimeout(timer); // clearTimeout, not cleartimeout..
+  }
 }
 
 // Gets the number of ms remaining to execute the event Function
 function getRemainingTime() {
-    return new Date().getTime() - startTimeMS;
+  return new Date().getTime() - startTimeMS;
 }
 
 function archiveFunction() {
-    isArchived ? (isArchived = false) : isArchived = true;
-    if (!isSwiped) {
-        $(selectedElement).removeClass("retract");
-        var isFinded = false;
-        var findedIndex;
-        archiveTaskId.forEach((idTask) => {
-            if (idTask == selectedElement.getAttribute("name")) {
-                findedIndex = archiveTaskId.indexOf(idTask);
-                isFinded = true;
-            }
-        });
+  if (archiveTaskId.length > 0) {
+    isArchived = true;
+  } else {
+    isArchived = false;
+  }
+  if (!isSwiped) {
+    $(selectedElement).removeClass("retract");
+    var isFinded = false;
+    var findedIndex;
+    archiveTaskId.forEach((idTask) => {
+      if (idTask == selectedElement.getAttribute("name")) {
+        findedIndex = archiveTaskId.indexOf(idTask);
+        isFinded = true;
+      }
+    });
 
-        if (isFinded) {
-            //On le déselectionne
-            selectedElement.style.border = "none";
-            archiveTaskId.splice(findedIndex, 1);
-            //Gestion des buttons.
-            archiveButtonManager();
-        } else {
-            selectedElement.style.border = "2px solid #f25f5c";
-            archiveTaskId.push(selectedElement.getAttribute("name"));
-            //Gestion des buttons.
-            archiveButtonManager();
-        }
+    if (isFinded) {
+      //On le déselectionne
+      selectedElement.style.border = "none";
+      archiveTaskId.splice(findedIndex, 1);
+      //Gestion des buttons.
+      archiveButtonManager();
     } else {
-        isSwiped = false;
-        isClickable = true;
-        timer = null;
+      selectedElement.style.border = "2px solid #f25f5c";
+      archiveTaskId.push(selectedElement.getAttribute("name"));
+      //Gestion des buttons.
+      archiveButtonManager();
     }
+  } else {
+    isSwiped = false;
+    isClickable = true;
+    timer = null;
+  }
 }
