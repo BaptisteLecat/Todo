f7a5499ab1bddff7c6096ec50e770957c65216be
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-03-16 15:58:29 +0100

    Update todoView.php

--- view/board/todoView.php
+++ view/board/todoView.php
@@ -12,7 +12,7 @@
       </div>
       <div class="color_number">
         <div class="number">
-          <h3>333</h3>
+          <h3><?= count($this->user->getList_Todo()); ?></h3>
         </div>
       </div>
     </div>
@@ -48,7 +48,7 @@
       </div>
       <div class="color_number">
         <div class="number">
-          <h3>333</h3>
+          <h3><?= count($this->user->getList_Todo()); ?></h3>
         </div>
       </div>
     </div>
