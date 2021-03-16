74ac7bfbebb541347f2516513f1943972dab44d0
BuildTools <unconfigured@null.spigotmc.org>
2021-03-16 22:59:41 +0100

    Create todoView.php

---
+++ view/board/todoView.php
@@ -0,0 +1,82 @@
+<?php ob_start(); ?>
+<main>
+
+  <div class="title">
+    <h1 class="name_title">Vos Todo</h1>
+  </div>
+  <div class="tt"></div>
+  <div class="information_todo">
+    <div class="test">
+      <div class="todo_title">
+        <h3 class="name_todo">Personnelle</h3>
+      </div>
+      <div class="color_number">
+        <div class="number">
+          <h3><?= count($this->user->getList_Todo()); ?></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class= "box_line">
+    <hr class="line">
+  </div>
+  <div class="container_todoView">
+    <?php foreach ($this->user->getList_Todo() as $todo) { ?>
+      <div class="box_todoView" onclick="document.location.href = 'board/<?= $todo->getId(); ?>'" id="<?= $todo->getId(); ?>">
+
+        <div class="box_icon">
+          <img class="img_theme" src="assets/icons/todo_icon/<?= $todo->getTodoIconObject()->getLabel(); ?>.png">
+        </div>
+
+        <div class="box_theme">
+          <h1 class="text_theme"><?= $todo->getTitle(); ?></h1>
+        </div>
+
+        <p><?= count($todo->getList_Task()); ?></p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+    <?php } ?>
+  </div>
+  <div class="tt"></div>
+  <div class="information_todo">
+    <div class="test">
+      <div class="todo_title">
+        <h3 class="name_todo">Groupe</h3>
+      </div>
+      <div class="color_number">
+        <div class="number">
+          <h3><?= count($this->user->getList_Todo()); ?></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class= "box_line">
+    <hr class="line">
+  </div>
+
+  <div class="container_todoView">
+    <?php foreach ($this->user->getList_TodoContribute() as $todo) { ?>
+      <div class="box_todoView" onclick="document.location.href = 'board/<?= $todo->getId(); ?>'" id="<?= $todo->getId(); ?>">
+
+        <div class="box_icon">
+          <img class="img_theme" src="assets/icons/todo_icon/<?= $todo->getTodoIconObject()->getLabel(); ?>.png">
+        </div>
+
+        <div class="box_theme">
+          <h1 class="text_theme"><?= $todo->getTitle(); ?></h1>
+        </div>
+
+        <p><?= count($todo->getList_Task()); ?></p>
+
+        <div class="progressBar">
+          <div class="line_progressBar"></div>
+        </div>
+      </div>
+    <?php } ?>
+
+  </div>
+</main>
+<?php $this->content = ob_get_clean(); ?>
