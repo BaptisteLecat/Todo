0d0307d797d110b22ac047443ee71f5e4f3d170f
Baptiste <baptiste.lecat44@gmail.com>
2021-03-16 22:58:09 +0100

    Gestion App et module
    Implementation class App - Gestion des Module sur todoContribute

--- App/Module/ModuleTaskManager.php
+++ App/Module/ModuleTaskManager.php
@@ -4,13 +4,15 @@ namespace App\Module;
 
 session_start();
 
-use App\Model\PriorityManager;
-use App\Model\TaskManager;
 use Exception;
+use App\Model\TaskManager;
+use App\Model\PriorityManager;
+use App\Model\ContributeManager;
 
 class ModuleTaskManager
 {
     private static $userObject;
+    private static $appObject;
 
     public static function archiveTask($list_idTask, $idTodo)
     {
@@ -18,6 +20,7 @@ class ModuleTaskManager
 
         try {
             self::$userObject = unserialize($_SESSION["User"]);
+            self::$appObject = unserialize($_SESSION["App"]);
             //Récupération de l'object associé à l'id todo passé en paramètre.
             $todoObject = self::getTodoObject($idTodo);
 
@@ -45,8 +48,8 @@ class ModuleTaskManager
         $task_return = null;
 
         try {
-            $list_priority = PriorityManager::loadPriority();
             self::$userObject = unserialize($_SESSION["User"]);
+            self::$appObject = unserialize($_SESSION["App"]);
             //Récupération de l'object associé à l'id todo passé en paramètre.
             $todoObject = self::getTodoObject($idTodo);
 
@@ -55,8 +58,8 @@ class ModuleTaskManager
                 if ($idTask == $taskObject->getId()) {
                     //Archivage
                     TaskManager::achieveTask(self::$userObject, $taskObject);
-                    TaskManager::reloadTask($taskObject, $list_priority);
-                    $task_return = TaskManager::reloadTask($taskObject, $list_priority);
+                    TaskManager::reloadTask($taskObject, self::$appObject->getList_Priority());
+                    $task_return = TaskManager::reloadTask($taskObject, self::$appObject->getList_Priority());
                     break;
                 }
             }
@@ -69,13 +72,47 @@ class ModuleTaskManager
 
     private static function getTodoObject($idTodo)
     {
+        self::loadUserTodoContribute(self::$userObject, self::$appObject->getList_TodoIcon(), self::$appObject->getList_Permission());
+        self::loadTodoContributeTask(self::$userObject, self::$appObject->getList_Priority());
+
         $todoObject = null;
+        $isFinded = false;
         foreach (self::$userObject->getList_Todo() as $todo) {
             if ($todo->getId() == $idTodo) {
                 $todoObject = $todo;
+                $isFinded = true;
                 break;
             }
         }
+
+        if (!$isFinded) {
+            foreach (self::$userObject->getList_TodoContribute() as $todo) {
+                if ($todo->getId() == $idTodo) {
+                    $todoObject = $todo;
+                    break;
+                }
+            }
+        }
         return $todoObject;
+    }
+
+    private static function loadUserTodoContribute($user, $list_todoIcon, $list_permission)
+    {
+        try {
+            ContributeManager::loadTodoContribute($user, $list_todoIcon, $list_permission);
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
+
+    private static function loadTodoContributeTask($user, $list_priority)
+    {
+        try {
+            foreach ($user->getList_TodoContribute() as $todoContribute) {
+                TaskManager::loadTask($todoContribute, $list_priority);
+            }
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
     }
 }
