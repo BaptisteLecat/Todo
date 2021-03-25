4c9379a7c70f749f7f0fef91c8b43a7b881e17d2
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-03-25 16:23:29 +0100

    Modification info Todo
    Affichage du button "Modifier" si le contenu des input change.

---
+++ public/js/settings/informations/formUpdate.js
@@ -0,0 +1,25 @@
+form_isModified = false;
+
+document.getElementsByTagName("form")[0].addEventListener("change", function 
+(event) {
+    if(!form_isModified){
+        form_isModified = true;
+        console.log(event.target);
+        showSubmitButton(event.target.parentNode.getElementsByClassName("formSubmit")[0]);
+    }
+});
+
+document.getElementsByClassName("formSubmit")[0].getElementsByTagName("button")[0].addEventListener("click", function(event) {
+    hideSubmitButton(event.target.parentNode.getElementsByClassName("formSubmit")[0]);
+})
+
+function hideSubmitButton(button) {
+    button.style.display = "none";
+    button.style.height = "0px";
+}
+
+
+function showSubmitButton(button) {
+    button.style.display = "flex";
+    button.style.height = "auto";
+}
