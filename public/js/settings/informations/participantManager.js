d862029ed0f07b25396e48e2cba74c927061da88
Baptiste <baptiste.lecat44@gmail.com>
2021-03-25 22:01:01 +0100

    Animation participant wrap

--- public/js/settings/informations/participantManager.js
+++ public/js/settings/informations/participantManager.js
@@ -7,13 +7,17 @@ $(".participant-develop_container").each(function() {
 function participantWrapManager(event) {
     element = event.target.parentNode.parentNode.parentNode;
     //Le participant est wrappé.
-    if(element.getElementsByClassName("participant-info_container")[0].style.display == "none"){
-        event.target.style.transform = "rotate(0deg)";
-        element.style.paddingBottom = "20px";
-        element.getElementsByClassName("participant-info_container")[0].style.display = "flex";
-    }else{
+    if(element.getElementsByClassName("participant-info_container")[0].style.display != "flex"){
         event.target.style.transform = "rotate(180deg)";
+        element.style.paddingBottom = "20px";
+        element.getElementsByClassName("participant-info_container")[0].style.maxHeight = "600px";
+        setTimeout(() => {
+            element.getElementsByClassName("participant-info_container")[0].style.display = "flex";
+        }, 200);
+    }else{
+        event.target.style.transform = "rotate(0deg)";
         element.style.paddingBottom = "10px";
+        element.getElementsByClassName("participant-info_container")[0].style.maxHeight = "0px";
         element.getElementsByClassName("participant-info_container")[0].style.display = "none";
     }
 }
