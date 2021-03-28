dc32563ee5c4b067d38b8ba7ee1106ab6eaad54f
Baptiste <baptiste.lecat44@gmail.com>
2021-03-28 22:54:47 +0200

    Fonctionnalité de modification permission
    Finalisation du fichier appelé en AJAX.

--- public/module/board/settings/informations/updatePermission/updateContributorPermission.php
+++ public/module/board/settings/informations/updatePermission/updateContributorPermission.php
@@ -1,7 +1,10 @@
 <?php
-
 require_once '../../../../../../vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+use App\Model\Entity\Permission;
 use App\Module\Settings\ModuleInformations;
 use App\Model\Exceptions\SuccessManager;
 use App\Model\Exceptions\PermissionException;
@@ -9,22 +12,23 @@ use App\Model\Exceptions\PermissionException;
 try {
     $messageBox = null;
     $success = null;
-    $todo = null;
+    $idTodo = null;
     $right = array();
+    $contributorObject = null;
 
     $right = $_POST["idPermission"];
-    $idContributor = $_POST["idContributor"]; //valeur des inputs
 
+    $idContributor = $_POST["idContributor"]; //valeur des inputs
     $idTodo = $_POST["idTodo"];
 
-    $givedPermission = ModuleInformations::updateUserRight($idTodo, intval($idContributor), $right);
+    $contributorObject = ModuleInformations::updateUserRight($idTodo, intval($idContributor), $right);
     $success = new SuccessManager("La mise à jour des permissions à réussie", "success");
     $success = $success->__toString();
+    //TODO retourner les list des permission pour actualiser la page.
 } catch (PermissionException $e) {
     $messageBox = $e->__toString();
-} catch (Error $e) {
+} catch (Exception $e) {
     throw new Exception($e);
 }
-
-$response = ["messageBox" => $messageBox, "success" => $success, "givedPermission" => $givedPermission];
+$response = ["messageBox" => $messageBox, "success" => $success, "contributorObject" => $contributorObject];
 echo json_encode($response);
