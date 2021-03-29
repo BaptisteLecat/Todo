51d6335e063bac5b796af40548a7c7c3675ec123
Baptiste <baptiste.lecat44@gmail.com>
2021-03-29 17:36:56 +0200

    Update todo.php
    Erreur de redirection.

--- view/board/todo.php
+++ view/board/todo.php
@@ -2,7 +2,7 @@
 <main>
     <header>
         <div class="header_side">
-            <img id="left-side_icon" src="assets\icons\left-arrow.png" class="previousPage" onclick="<?= $this->goBack() ?>">
+            <img id="left-side_icon" src="assets\icons\left-arrow.png" class="previousPage" onclick="document.location.href = '<?= $this->goBack() ?>'">
         </div>
 
         <div class="header_middle">
