9ca02887f8ca254980e7a54ece221fa03d9a083f
Baptiste <baptiste.lecat44@gmail.com>
2021-04-04 23:07:49 +0200

    Update login.php
    Appel des fonctions de connection

--- public/controllers/form/login.php
+++ public/controllers/form/login.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Model\Exceptions\SuccessManager;
 use App\Model\Form\Sign\SignIn;
 
 session_destroy();
@@ -7,7 +8,17 @@ session_start();
 
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-    $signIn = new SignIn($_POST["email"], $_POST["password"]);
+    try{
+        $signIn = new SignIn($_POST["email"], $_POST["password"]);
+        $_SESSION["User"] = serialize($signIn->signIn());
+        header('refresh:2.3;url=home');
+
+        $successMessage = new SuccessManager("Succès de l'authentification ! ", "success");
+        $this->messageBox = $successMessage;
+    }catch (Exception $e) {
+        $this->messageBox = $e;
+    }
+    //TODO LE Systeme de gestion des erreurs n'est pas fonctionnelle pour les erreurs native, qui ne s'afficheront pas en messageBox.
 }
 
 include '../view/form/login.php';
