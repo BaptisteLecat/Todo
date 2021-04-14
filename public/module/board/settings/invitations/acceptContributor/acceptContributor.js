27872046f05b9a20bff6cbb6fa86f0256861025a
Baptiste <baptiste.lecat44@gmail.com>
2021-04-14 22:35:38 +0200

    Fonctionnalité acceptContributor
    Permet d'accepter la demande de participation d'un user qui a saisie un token correspondant à la todo.

---
+++ public/module/board/settings/invitations/acceptContributor/acceptContributor.js
@@ -0,0 +1,39 @@
+function acceptContributor(idContributor, idTodo) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.response;
+            if (response["success"] != null) {
+                document.getElementsByClassName(
+                    "invitation_wrapper"
+                )[0].innerHTML = displayInvitations(
+                    response["list_invitation"],
+                    idTodo
+                );
+                createMessageBox(response["success"]);
+            } else {
+                if (response["messageBox"] != null) {
+                    createMessageBox(response["messageBox"]);
+                }
+            }
+        } else if (this.readyState == 4) {
+            alert("Une erreur est survenue..");
+        } else if (this.statusText == "parsererror") {
+            alert("Erreur Json");
+        }
+    };
+
+    xhr.open(
+        "POST",
+        "module/board/settings/invitations/acceptContributor/acceptContributor.php",
+        true
+    );
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(
+        "&idContributor=" +
+        encodeURI(idContributor) +
+        "&idTodo=" +
+        encodeURI(idTodo)
+    );
+}
