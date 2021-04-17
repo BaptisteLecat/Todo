622767ee4daf453a2833c4616ff091aef4f42154
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-04-17 13:40:56 +0200

    Update achieve.php
    Réinitialisation du fichier.

--- public/module/task/achieve/achieve.php
+++ public/module/task/achieve/achieve.php
@@ -1,37 +1,22 @@
+
 <?php
 
-//require_once '../../../../../vendor/autoload.php';
+require_once '../../../../vendor/autoload.php';
 
-echo("coucou");
-/*use App\Model\Exceptions\SuccessManager;
 use App\Model\Exceptions\PermissionException;
-use App\Module\Settings\ModuleHome;
+use App\Module\Todo\ModuleTaskManager;
 
 try {
     $messageBox = null;
-    $success = null;
-    $list_token = null;
+    $task = null;
 
-    if (isset($_POST["idTask"]) && !is_null($_POST["idTask"])) {
-        $idContributor = intval($_POST["idTask"]);
-    } else {
-        throw new Exception("Cette tâche n'existe pas.");
-    }
+    $idTask = $_POST["idTask"];
+    $idTodo = $_POST["idTodo"];
 
-    if (isset($_POST["idTodo"]) && !is_null($_POST["idTodo"])) {
-        $idTodo = intval($_POST["idTodo"]);
-    } else {
-        throw new Exception("Cette todo n'existe pas.");
-    }
-
-    $task = ModuleHome::achieveTask($idTask, $idTodo);
-    $success = new SuccessManager("L'état de la tâche a été modifié.", "success");
-    $success = $success->__toString();
+    $task = ModuleTaskManager::achieveTask($idTask, $idTodo);
 } catch (PermissionException $e) {
     $messageBox = $e->__toString();
-} catch (Exception $e) {
-    throw new Exception($e);
 }
 
-$response = ["messageBox" => $messageBox, "success" => $success, "task" => $task];
-echo json_encode($response);*/
+$response = ["messageBox" => $messageBox, "task" => $task];
+echo json_encode($response);
