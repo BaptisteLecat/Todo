41d6656a3700a06352f785df9baefead0e6757fb
Baptiste <baptiste.lecat44@gmail.com>
2021-04-30 00:07:31 +0200

    Create FormTask.php
    Cette classe permet la gestion du formulaire des tâches.

---
+++ App/Model/Form/CreateForm/FormTask.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\App;
+use App\Model\Form\Form;
+use App\Model\Entity\User;
+use App\Model\Exceptions\FormException;
+use App\Model\Exceptions\PermissionException;
+use App\Model\TaskManager;
+
+class FormTask extends Form
+{
+    private $title;
+    private $content;
+    private $endDate;
+    private $idTodo;
+    private $idPriority;
+
+    public function __construct(string $title, string $content, string $endDate, int $idTodo, int $idPriority, User $user, App $app)
+    {
+        parent::__construct($user, $app);
+        $this->title = $title;
+        $this->content = $content;
+        $this->endDate = $endDate;
+        $this->idTodo = $idTodo;
+        $this->idPriority = $idPriority;
+    }
+
+    public function submitForm()
+    {
+        $list_input = array("title_task" => $this->title, "content_task" => $this->content, "endDate_task" => $this->endDate);
+
+        //Verification de la syntaxe des champs.
+        if ($this->verifInput($list_input)) {
+            //Verification de l'existance des id passé en paramètres. Récupération des objects associés.
+            $todoObject = $this->getTodoObject($this->idTodo);
+            $priorityObject = $this->getPriorityObject($this->idPriority);
+            if (!is_null($todoObject)) {
+                if (!is_null($priorityObject)) {
+                    //Verification des droits de l'utilisateur.
+                    if ($todoObject->getOwned() || ($todoObject->havePermissionTo(3) && $this->user->isAcceptedInTodo($todoObject))) {
+                        //Insertion des informations.
+                        TaskManager::insertTask($this->title, $this->content, $this->endDate, $priorityObject, $todoObject, $this->user);
+                    } else {
+                        throw new PermissionException(1);
+                    }
+                } else {
+                    throw new FormException(1);
+                    
+                }
+            } else {
+                throw new FormException(0);
+            }
+        }
+    }
+}
