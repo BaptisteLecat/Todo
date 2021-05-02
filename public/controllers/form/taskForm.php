982fedc5b0f2dc2bd0f7375a28356b3416cef498
Baptiste <baptiste.lecat44@gmail.com>
2021-05-02 15:27:23 +0200

    Redesign du formulaire
    Refonte total du HTML CSS de la page form Task.

--- public/controllers/form/taskForm.php
+++ public/controllers/form/taskForm.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Model\Utils\MessageBox;
-
+use App\Model\Form\CreateForm\FormTask;
+use App\Model\Exceptions\SuccessManager;
+/*
 if (isset($_POST["todo-selector"])) { //Si le formulaire a été soumis.
     $errorId = true; //Variable permettant de verifier que l'id du todo-selector existe bien.
     foreach ($this->user->getList_Todo() as $todo) {
@@ -24,5 +26,36 @@ if (isset($_POST["todo-selector"])) { //Si le formulaire a été soumis.
     if ($errorId == true) {
         $this->messageBox = new MessageBox("Ohoh, le Todo sélectionné est inconnu !", "error");
     }
+}*/
+
+const INPUT_ARGS = array(
+    'title' => FILTER_SANITIZE_STRING,
+    'description' => FILTER_SANITIZE_STRING,
+    'endDate' => FILTER_SANITIZE_STRING,
+    'todo-selector' => array(
+        'filter' => FILTER_VALIDATE_INT,
+        'options' => array('min_range' => 0)
+    ),
+    'priority-selector' => array(
+        'filter' => FILTER_VALIDATE_INT,
+        'options' => array('min_range' => 0)
+    )
+);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $filtered_input = filter_input_array(INPUT_POST, INPUT_ARGS);
+    if(!is_null($filtered_input) && $filtered_input == true){
+        try {
+            $formTask = new FormTask($filtered_input["title"], $filtered_input["description"], $filtered_input["endDate"], $filtered_input["todo-selector"], $filtered_input["priority-selector"], $this->user, $this->app);
+            $formTask->submitForm();
+            $successMessage = new SuccessManager("La tâche à été ajoutée. ", "success");
+            $this->messageBox = $successMessage;
+        } catch (Exception $e) {
+            $this->messageBox = $e;
+        }
+    }
 }
+
+
 include("../view/form/taskForm.php");
--- view/form/taskForm.php
+++ view/form/taskForm.php
@@ -1,32 +1,50 @@
 <?php ob_start(); ?>
 <main>
     <header>
-        <img src="assets\icons\left-arrow.png" class="previousPage" onclick="document.location.href='home'">
-        <div class="header_title">
-            <h1>Création d'une Tâche</h1>
+        <div class="header_top">
+            <img id="left-side_icon" src="..\..\assets\icons\left-arrow.png" class="previousPage" onclick="document.location.href='<?= $this->goPrevious() ?>'">
+        </div>
+
+        <div class="header_bottom">
+            <h1>Ajout d'une tâche</h1>
         </div>
     </header>
-    <form method="post">
-        <div class="form_switcher" onclick="document.location.href='form/createtodo'">
-            <h3>Ajouter un Todo</h3>
+    <form method="POST">
+        <div class="input_container">
+            <label>Todo de référence</label>
+            <select name="todo-selector" id="todo">
+                <?php foreach ($this->user->getList_Todo() as $todo) { ?>
+                    <option value="<?= $todo->getId() ?>"><?= $todo->getTitle() ?></option>
+                <?php } ?>
+            </select>
         </div>
-        <h6>Todo de référence</h6>
-        <select name="todo-selector" id="todo">
-            <?php foreach ($this->user->getList_Todo() as $todo) { ?>
-                <option value="<?= $todo->getId() ?>"><?= $todo->getTitle() ?></option>
-            <?php } ?>
-        </select>
 
-        <h6>Contenu de la Tâche</h6>
-        <textarea name="content" required></textarea>
+        <div class="input_container">
+            <label>Titre</label>
+            <input type="text" name="title" placeholder="Faire les courses.." required></input>
 
-        <div class="titleInput_container">
-            <h6>Date de fin</h6>
-            <img src="assets\icons\information.png">
         </div>
-        <input class="bug" name="date" type="date" value="<?= date('Y-m-d'); ?>">
+        <div class="input_container">
+            <label>Contenu</label>
+            <textarea name="content" placeholder="Acheter les aliments pour le repas.." required></textarea>
 
-        <input type="submit" value="Valider">
+        </div>
+        <div class="input_container">
+            <label>A réaliser avant le</label>
+            <input name="endDate" type="date" value="<?= date('Y-m-d'); ?>">
+        </div>
+        <div class="input_container">
+            <label>Priorité</label>
+            <select name="priority-selector" id="priority">
+                <?php foreach ($this->app->getList_Priority() as $priority) { ?>
+                    <option value="<?= $priority->getId() ?>"><?= $priority->getLabel() ?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <div class="input_container">
+            <button type="submit">Rajouter du boulot</button>
+            <input type="button" value="Créer une Todo"></input>
+        </div>
     </form>
 </main>
 
