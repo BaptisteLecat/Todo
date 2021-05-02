665b5bb29b38da75da06e7e592cf80c7bec89c13
Baptiste <baptiste.lecat44@gmail.com>
2021-05-03 00:14:10 +0200

    Update taskForm.php

--- public/controllers/form/taskForm.php
+++ public/controllers/form/taskForm.php
@@ -5,7 +5,7 @@ use App\Model\Exceptions\SuccessManager;
 
 const INPUT_ARGS = array(
     'title' => FILTER_SANITIZE_STRING,
-    'description' => FILTER_SANITIZE_STRING,
+    'content' => FILTER_SANITIZE_STRING,
     'endDate' => FILTER_SANITIZE_STRING,
     'todo-selector' => array(
         'filter' => FILTER_VALIDATE_INT,
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $filtered_input = filter_input_array(INPUT_POST, INPUT_ARGS);
     if(!is_null($filtered_input) && $filtered_input == true){
         try {
-            $formTask = new FormTask($filtered_input["title"], $filtered_input["description"], $filtered_input["endDate"], $filtered_input["todo-selector"], $filtered_input["priority-selector"], $this->user, $this->app);
+            $formTask = new FormTask($filtered_input["title"], $filtered_input["content"], $filtered_input["endDate"], $filtered_input["todo-selector"], $filtered_input["priority-selector"], $this->user, $this->app);
             $formTask->submitForm();
             $successMessage = new SuccessManager("La tâche à été ajoutée. ", "success");
             $this->messageBox = $successMessage;
