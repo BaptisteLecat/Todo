b3b370e9f56c6dae89c8b7912c4f87d733943adb
Baptiste <baptiste.lecat44@gmail.com>
2021-05-07 20:03:23 +0200

    Update signIn.php
    Sécurisation faille XSS

--- public/controllers/form/signIn.php
+++ public/controllers/form/signIn.php
@@ -7,23 +7,32 @@ use App\Model\Exceptions\SuccessManager;
 session_destroy();
 session_start();
 
-if (isset($_POST['email']) && isset($_POST['password'])) {
-    try{
-        $signIn = new SignIn($_POST["email"], $_POST["password"]);
-        $_SESSION["User"] = serialize($signIn->signIn());
-        header('refresh:2.3;url=home');
+const INPUT_ARGS = array(
+    'email' => FILTER_VALIDATE_EMAIL,
+    'password' => FILTER_SANITIZE_STRING
+);
 
-        
-        if (VisitorCounter::getInstanceCount() == 0) {
-            $visitor = new VisitorCounter();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $filtered_input = filter_input_array(INPUT_POST, INPUT_ARGS);
+    if (!is_null($filtered_input) && $filtered_input == true) {
+        try {
+            $signIn = new SignIn($filtered_input["email"], $filtered_input["password"]);
+            $_SESSION["User"] = serialize($signIn->signIn());
+            header('refresh:2.3;url=home');
+
+
+            if (VisitorCounter::getInstanceCount() == 0) {
+                $visitor = new VisitorCounter();
+            }
+
+            $successMessage = new SuccessManager("Succès de l'authentification ! ", "success");
+            $this->messageBox = $successMessage;
+        } catch (Exception $e) {
+            $this->messageBox = $e;
         }
-
-        $successMessage = new SuccessManager("Succès de l'authentification ! ", "success");
-        $this->messageBox = $successMessage;
-    }catch (Exception $e) {
-        $this->messageBox = $e;
-    }
     //TODO LE Systeme de gestion des erreurs n'est pas fonctionnelle pour les erreurs native, qui ne s'afficheront pas en messageBox.
+    }
 }
 
 include '../view/form/signIn.php';
