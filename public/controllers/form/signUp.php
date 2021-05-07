d8447a73b2174cab2096a25f146b8811dcf1c5e8
Baptiste <baptiste.lecat44@gmail.com>
2021-05-07 20:29:26 +0200

    Update signUp.php
    Sécurisation faille XSS

--- public/controllers/form/signUp.php
+++ public/controllers/form/signUp.php
@@ -15,41 +15,56 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-if (isset($_POST['name']) && isset($_POST['firstname'])) {
-    try{
-        
-        $etape = 1;
-        $signUp = new SignUp($_POST["name"], $_POST["firstname"]);
-        $etape = ($signUp->validFirstStep()) ? 2 : 1;
-        $_SESSION["SignUp"] = serialize($signUp);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['name']) && isset($_POST['firstname'])) {
+        $INPUT_ARGS = array(
+            'name' => FILTER_SANITIZE_STRING,
+            'firstname' => FILTER_SANITIZE_STRING
+        );
+        $filtered_input = filter_input_array(INPUT_POST, $INPUT_ARGS);
+        if (!is_null($filtered_input) && $filtered_input == true) {
+            try {
 
-    }catch (Exception $e) {
-        $this->messageBox = $e;
-        $etape = 1;
-    }
-}else{
-    if (isset($_POST['email']) && isset($_POST['password'])) {
-        try {
-
-            if(isset($_SESSION["SignUp"])){
-                $etape = 2;
-                $signUp = unserialize($_SESSION["SignUp"]);
-                $etape = $signUp->signUp($_POST['email'], $_POST['password']);
-                header('refresh:2.3;url=login');
-
-                $successMessage = new SuccessManager("Succès de l'authentification ! ", "success");
-                $this->messageBox = $successMessage;
-            }else{
-                //throw new Exception("Une erreur est survenue !");
+                $etape = 1;
+                $signUp = new SignUp($filtered_input["name"], $filtered_input["firstname"]);
+                $etape = ($signUp->validFirstStep()) ? 2 : 1;
+                $_SESSION["SignUp"] = serialize($signUp);
+            } catch (Exception $e) {
+                $this->messageBox = $e;
                 $etape = 1;
             }
-        } catch (Exception $e) {
-            $this->messageBox = $e;
-            $etape = 2;
         }
-    }else{
-        $etape = 1;
+    } else {
+        if (isset($_POST['email']) && isset($_POST['password'])) {
+            $INPUT_ARGS = array(
+                'email' => FILTER_VALIDATE_EMAIL,
+                'password' => FILTER_SANITIZE_STRING
+            );
+            $filtered_input = filter_input_array(INPUT_POST, $INPUT_ARGS);
+            if (!is_null($filtered_input) && $filtered_input == true) {
+                try {
+
+                    if (isset($_SESSION["SignUp"])) {
+                        $etape = 2;
+                        $signUp = unserialize($_SESSION["SignUp"]);
+                        $etape = $signUp->signUp($filtered_input['email'], $filtered_input['password']);
+                        header('refresh:2.3;url=login');
+
+                        $successMessage = new SuccessManager("Succès de l'authentification ! ", "success");
+                        $this->messageBox = $successMessage;
+                    } else {
+                        //throw new Exception("Une erreur est survenue !");
+                        $etape = 1;
+                    }
+                } catch (Exception $e) {
+                    $this->messageBox = $e;
+                    $etape = 2;
+                }
+            }
+        }
     }
+} else {
+    $etape = 1;
 }
 
 include '../view/form/signUp.php';
