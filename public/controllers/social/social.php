5dcc0f80bc79529728ef72d37d2e4e1c4f9a073f
Baptiste <baptiste.lecat44@gmail.com>
2021-05-20 00:22:17 +0200

    Lien d'insertion token depuis url
    Récupération GET du token, pour simplifier l'ajout dans une todo.

--- public/controllers/social/social.php
+++ public/controllers/social/social.php
@@ -1,6 +1,28 @@
 <?php
 
+use App\Module\Social\ModuleSocial;
+use App\Model\Exceptions\SuccessManager;
+use App\Model\Exceptions\DatabaseException;
 use App\Model\PendingContributeManager;
+
+try {
+    $list_pendingContribute = null;
+    
+    if (isset($_GET["token"]) && !is_null($_GET["token"])) {
+        $token = $_GET["token"];
+        $list_pendingContribute = ModuleSocial::submitToken($token);
+    }
+    
+    
+    $success = new SuccessManager("Votre demande est désormais en attente.", "success");
+    $this->messageBox = $success->__toString();
+} catch (PDOException $e) {
+    $this->messageBox = $e->__toString();
+} catch (DatabaseException $e) {
+    $this->messageBox = $e->__toString();
+} catch (Exception $e) {
+    throw new Exception($e);
+}
 
 $list_pendingContribute = PendingContributeManager::loadPendingContribute($this->user);
 
